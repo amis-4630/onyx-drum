7b46cbb7dc3c2e80b645aaa6d8bfbad9f9a0661c
Chad Thomas <chad.thomas@neudesic.com>
2024-04-11 21:11:56 -0400

    clean up unused elements

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -2,8 +2,6 @@ import {useRoutes, BrowserRouter as Router} from 'react-router-dom';
 
 import './App.css';
 import Home from './Pages/Home';
-import MyAccount from './Pages/MyAccount';
-import MyOrder from './Pages/MyOrder';
 import MyOrders from './Pages/MyOrders';
 import NotFound from './Pages/NotFound';
 import SignIn from './Pages/SignIn';
@@ -12,7 +10,6 @@ import Navbar from './Components/Navbar/Index';
 const AppRoutes = () => {
   const routes = useRoutes([
     {path: '/', element: <Home/>},
-    {path: '/my-account', element: <MyAccount/>},
     {path: '/my-orders', element: <MyOrders/>},
     {path: '/sign-in', element: <SignIn/>},
     {path: '*', element: <NotFound/>}
