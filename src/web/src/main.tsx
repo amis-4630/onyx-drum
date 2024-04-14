d9635c10b079682952f6a239710a1a3503b8ea0b
Chad Thomas <chad.thomas@neudesic.com>
2024-04-14 12:11:02 -0400

    adds Auth provider

--- src/web/src/main.tsx
+++ src/web/src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Auth0Provider
+    domain="dev-uzzrp67rzhx2mm8n.us.auth0.com"
+    clientId="k07Sf0W04o5U0N2O2VCukPfM3BthdWvf"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>,
 )
