2ee6434689e16b73fae924b7d00e99ab20bfa444
Chad Thomas <chad.thomas@neudesic.com>
2024-04-16 18:55:38 -0400

    finish product service

--- src/web/src/Services/CatalogService/index.tsx
+++ src/web/src/Services/CatalogService/index.tsx
@@ -1,19 +1,22 @@
-import React, {useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 import { Product, emptyProducts } from '../../Models/Product';
 
 const CatalogService = () => {
-    const [products, setProducts]: [Product[], (products: Product[]) => void] = useState(emptyProducts);
+    const [products, setProducts] = useState(emptyProducts);
 
     useEffect(() => {
-        axios.get<Product[]>("http://http://localhost:5264/api/catalog/",
-            { 
-                headers: { 
-                    "Content-Type": "application/json", 
-                },
-            })
-            .then((response) => setProducts(response.data))
-            .catch((error)) => console.log(error));
+        axios.get<Product[]>("http://localhost:5264/api/catalog/", { 
+            headers: { 
+                "Content-Type": "application/json", 
+            },
+        })
+        .then((response) => setProducts(response.data))
+        .catch((error) => console.error(error));
     }, []);
+
+    return products;
 }
+
+export default CatalogService;
 
